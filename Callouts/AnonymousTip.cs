056cc174aa3e03a5f0e422818145cf30b47d9bcb
Supopur <matylda.smyk.09@gmail.com>
2023-06-03 21:38:09 +0200

    Added more comments to make this mess more readable

--- Callouts/AnonymousTip.cs
+++ Callouts/AnonymousTip.cs
@@ -16,6 +16,7 @@ namespace MysteriousCallouts.Callouts
     [CalloutInterface("Anonymous Tip", CalloutProbability.Low, "Tip of a location of a wanted felon.", "Code 3", "FIB IAA")]
     internal class AnonymousTip : Callout
     {
+        // Variables
         internal static Random rndm = new Random(DateTime.Now.Millisecond);
         internal static bool SuccessfulIPPing = true;
         internal static bool SuccessfulDecryption = true;
@@ -26,71 +27,104 @@ namespace MysteriousCallouts.Callouts
 
         internal const string lettermsg =
             "You made it this far. I am shocked. These new gen z cops be kinda dog shit. They do not read. They probably won't even read this. You must be some oldie actual good cop. Anyways. Back to business. I have a hostage. If you want to see them alive, you better find me. Your final clue has been copied to your computer. Good Luck";
+
+        // Callout Setup
         public override bool OnBeforeCalloutDisplayed()
         {
             CalloutMessage = "Anonymous Tip";
             CalloutPosition = Game.LocalPlayer.Character.Position;
             return base.OnBeforeCalloutDisplayed();
         }
-        
+
+        // Callout Process
         public override void Process()
         {
+            // Resume game if console is open
             if (Game.Console.IsOpen)
             {
                 Game.IsPaused = false;
             }
             base.Process();
         }
+
+        // Callout Accepted
         public override bool OnCalloutAccepted()
         {
+            // Get encrypted IP and copy it to the clipboard
             EncryptedIP = IPHelper.GetEncryptedIP();
             Game.SetClipboardText(EncryptedIP);
-            Logger.Normal("OnCalloutAccepted() in AnonymousTip.cs",$"Encrypted IP: {EncryptedIP}");
+
+            // Log and display notification with the tip message and encrypted IP
+            Logger.Normal("OnCalloutAccepted() in AnonymousTip.cs", $"Encrypted IP: {EncryptedIP}");
             string tipMSG = $"{KidnappingEvent.GetRandomTip()} Complete the objective or suffer the consequences. Here is your first clue: {EncryptedIP}";
             msg = tipMSG;
-            Game.DisplayNotification("mparcadecabinetgrid","phone_anim_1","NOTIFICATION",$"By {KidnappingEvent.GetRandomPhoneNumber()}",tipMSG);
+            Game.DisplayNotification("mparcadecabinetgrid", "phone_anim_1", "NOTIFICATION", $"By {KidnappingEvent.GetRandomPhoneNumber()}", tipMSG);
+
+            // Initiate the callout
             Callout();
             return base.OnCalloutAccepted();
         }
 
+        // Callout Not Accepted
         public override void OnCalloutNotAccepted()
         {
             Logger.Normal("OnCalloutNotAccepted() in AnonymousTip.cs", "Callout not accepted by user");
             base.OnCalloutNotAccepted();
         }
 
+        // Callout Method
         internal void Callout()
         {
             GameFiber.StartNew(delegate
             {
+                // Start the decryption process
                 StartDecryptionProcess();
-                if(BrokenDownVehicleEvent.FindSpawnPoint());
+
+                // Check if a broken down vehicle spawn point can be found
+                if (BrokenDownVehicleEvent.FindSpawnPoint())
                 {
+                    // Spawn and trigger the broken down vehicle event
                     BrokenDownVehicleEvent.Spawning();
                     BrokenDownVehicleEvent.MainEvent();
                 }
+
+                // End the callout
                 End();
             });
         }
-        
-        internal static bool IsIPPIngSuccessful() => SuccessfulIPPing;
+
+        // Check if the IPPing process was successful
+        internal static bool IsIPPingSuccessful() => SuccessfulIPPing;
+
+        // Check if the decryption process was successful
         internal static bool IsDecryptionSuccessful() => SuccessfulDecryption;
-        
+
+        // Start the decryption process
         internal static void StartDecryptionProcess()
         {
-                IPHelper.HelpWithDecryption();
-                while(!IsDecryptionSuccessful()){GameFiber.Wait(0);}
-                IPHelper.HelpWithIPPing();
-                while(!IsIPPIngSuccessful()){GameFiber.Wait(0);}
-                /*SuccessfulIPPing = false;
-                SuccessfulDecryption = false;
-                */
+            IPHelper.HelpWithDecryption();
+
+            // Wait until decryption is successful
+            while (!IsDecryptionSuccessful())
+            {
+                GameFiber.Wait(0);
+            }
+
+            IPHelper.HelpWithIPPing();
+
+            // Wait until IPPing is successful
+            while (!IsIPPingSuccessful())
+            {
+                GameFiber.Wait(0);
+            }
+
+            /*SuccessfulIPPing = false;
+            SuccessfulDecryption = false;
+            */
         }
-            
 
-
-
-        internal static void DeleteALlBlips()
+        // Delete all blips
+        internal static void DeleteAllBlips()
         {
             foreach (Blip b in AllBlips)
             {
@@ -102,10 +136,12 @@ namespace MysteriousCallouts.Callouts
                 {
                     continue;
                 }
-                
+
             }
         }
-        internal static void DismissALlPeds()
+
+        // Dismiss all peds
+        internal static void DismissAllPeds()
         {
             foreach (Ped p in AllPeds)
             {
@@ -117,15 +153,16 @@ namespace MysteriousCallouts.Callouts
                 {
                     continue;
                 }
-                
+
             }
         }
 
+        // End the callout
         public override void End()
         {
-            DeleteALlBlips();
-            DismissALlPeds();
-            Logger.Normal("End() in AnonymousTip.cs","Ending anonymous tip callout");
+            DeleteAllBlips();
+            DismissAllPeds();
+            Logger.Normal("End() in AnonymousTip.cs", "Ending anonymous tip callout");
             base.End();
         }
     }
